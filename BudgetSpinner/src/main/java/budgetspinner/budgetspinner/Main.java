a74d63b7d3a43fafa3753ffc31ec9edc8e39ba1d
JimiUrsin <jimi.ursin@gmail.com>
2018-11-20 03:48:34 +0200

    Main.java lisätty, gitignore päivitetty

---
+++ BudgetSpinner/src/main/java/budgetspinner/budgetspinner/Main.java
@@ -0,0 +1,14 @@
+
+package budgetspinner.budgetspinner;
+
+/**
+ *
+ * @author Jimi Ursin
+ */
+public class Main {
+    // TODO Parempi nimi luokalle?
+    
+    public static void main(String[] args) {
+        
+    }
+}
