fd9e801d2cc88b8ed362f611f6cf451c3334f2cf
JimiUrsin <jimi.ursin@gmail.com>
2018-11-27 19:27:59 +0200

    .gitignore edit, first time check fix

--- BudgetSpinner/src/main/java/budgetspinner/Main.java
+++ BudgetSpinner/src/main/java/budgetspinner/Main.java
@@ -14,9 +14,10 @@ public class Main {
     
     public static void main(String[] args) throws Exception { 
         
-        // If data.txt doesn't exist, we know it's the first time this application is being run
+        // If data.txt or running.txt doesn't exist, we know it's the first time this application is being run
         File db = new File("data.txt");
-        if (!db.exists()) {
+        File running = new File("running.txt");
+        if (!db.exists() || !running.exists()) {
             FirstTimeSetup.launch(FirstTimeSetup.class);            
         } else {
             MainView.launch(MainView.class);
